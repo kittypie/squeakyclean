436b445197a0f3693fdf3f4f7f3a266bb8236f84
kittypie <mannat.hira@gmail.com>
2019-12-16 00:16:42 +0000

    added camera with ldr

--- start-sc.py
+++ start-sc.py
@@ -1,23 +1,46 @@
 import RPi.GPIO as GPIO
 import time
-from datetime import date
+import traceback
+from datetime import datetime
+from gpiozero import LightSensor
+from picamera import PiCamera
 
 GPIO.setmode(GPIO.BCM)
+##
+##GPIO.setup(23, GPIO.IN) #PIR
+##GPIO.setup(24, GPIO.OUT) #BUzzer
 
-GPIO.setup(23, GPIO.IN) #PIR
-GPIO.setup(24, GPIO.OUT) #BUzzer
+ldr = LightSensor(17)
+camera = PiCamera()
+camera.rotation = 90
 
 try:
     time.sleep(2) # to stabilize sensor
     while True:
-        if GPIO.input(23):
-            GPIO.output(24, True)
-            time.sleep(0.1) #Buzzer turns on for 0.5 sec
-            GPIO.output(24, False)
+        print(ldr.value)
+        if ldr.value < 0.5:
             print("Motion Detected...Dispensing the toothpaste")
-           print("Start : %s" , time.ctime() )
-            time.sleep(5) #to avoid multiple detection
-        time.sleep(0.1) #loop delay, should be less than detection delay
+            camera.start_preview()
+            t = datetime.now()
+            timestamp=t.strftime('%b-%d-%Y_%H%M%S')
+            print("Started at " + timestamp)
+ 
+##            print("Start : " + timestamp)
+##            camera.capture('/home/pi/python/squeakyclean/photos/squeaky-' + timestamp + '.jpg')
+            time.sleep(2)
+##            #print("Start : " , datetime.now())
+            camera.stop_preview()
+        else:
+            print("No one brushing ... ")
+##        if GPIO.input(23):
+##            GPIO.output(24, True)
+##            time.sleep(0.1) #Buzzer turns on for 0.5 sec
+##            GPIO.output(24, False)
+##            print("Motion Detected...Dispensing the toothpaste")
+##            #print("Start : " , datetime.now())
+        time.sleep(1) #to avoid multiple detection
+##        time.sleep(0.1) #loop delay, should be less than detection delay
 
-except:
-    GPIO.cleanup()
+except Exception:
+    print("Exit squeaky clean ... ")
+    traceback.print_exc()
